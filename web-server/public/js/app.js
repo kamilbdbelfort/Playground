bdc7d16597503b1833561d7b7b300b28b136643b
Kamil Beeli de Belfort <k.beelidebelfort@hotmail.com>
2022-05-04 11:36:47 +0200

    weather app complete

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,27 @@
 console.log('File is loaded');
 
-fetch(`http://localhost:3000/weather?address=Hilversum`).then((response) => {
-  response.json().then((data) => {
-    if(data.error){
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+
+messageOne.textContent = 'From Javascript';
+
+const messageTwo = document.querySelector('#message-2');
+messageTwo.textContent = '';
+
+weatherForm.addEventListener('submit', (e) => {
+  messageTwo.textContent = 'Loading Message';
+  e.preventDefault();
+  fetch(`http://localhost:3000/weather?address=${search.value}`).then((response) => {
+    response.json().then((data) => {
+      if(data.error){
+        console.log(data.error);
+        messageTwo.textContent = data.error;
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+        messageTwo.textContent = data.location + ". " + data.forecast;
+      }
+    });
   });
-});
+})
